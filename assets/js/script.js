c66359a18082330b6811d046489d4f820902e73e
MichealHamoud <micheal.hamoud11@outlook.com>
2023-04-27 21:48:27 +1000

    fix some issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,209 +1,54 @@
-//Declare global Vars
-var $SearchBtn = $('#SearchBtn');
-var $ClearBtn = $('#Clear');
-var Input = document.querySelector('#search-city');
-var CityName = document.querySelector('#CityName');
-var Temp = document.querySelector('#Temp');
-var Wind = document.querySelector('#Wind');
-var Humid = document.querySelector('#Humidity');
-var Index = document.querySelector('#Index');
-var BtnPlace = $('#BtnSpots');
-let createImage= document.createElement('img');
-let createImage2= document.createElement('img');
-let createImage3= document.createElement('img');
-var HistoryCities = $(JSON.parse(localStorage.getItem('saved')));
+function GetInfo() {
 
-//assign UserInput to id of textbox
-addEntry();
-function addEntry() {
-    HistoryCities.sort();
-    for (var i = 0; i < HistoryCities.length; i++){
-        if(HistoryCities[i] === HistoryCities[i -1]){
-            HistoryCities.splice(i, 1);
-            i--;
-        }
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=20516ee7fdcc8a804bdcda9f63d8d30a')
+.then(response => response.json())
+.then(data => {
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+
     }
- BTNCreator();
-}
 
-function BTNCreator(){
-    BtnPlace.children().remove();
-    for(var i = 0; i <HistoryCities.length; i++){
-        var btn = document.createElement("button");
-        btn.textContent = (HistoryCities[i]);
-        btn.setAttribute('id', 'Relook');
-        document.getElementById('BtnSpots').appendChild(btn);
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
     }
-}
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+    console.log(data)
 
 
-
-
-
-
-$SearchBtn.on('click', function (event) {
-    console.log(Input.value);
-    
-    
-
-    // Fetch request to get current citys info
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + Input.value + '&appid=20516ee7fdcc8a804bdcda9f63d8d30a')
-        .then(response => response.json())
-        .then(data => {
-            //console.log(data);
-            var CityValue = data['name'];
-            var TemperValue = data['main']['temp'];
-            var windValue = data['wind']['speed'];
-            var HumidValue = data['main']['humidity'];
-            var ImgValue = data.weather[0].icon;
-
-
-            CityName.innerHTML = (CityValue + " ");
-            var convertTemp = Math.trunc(1.8 * (TemperValue - 273) + 32);
-            Temp.innerHTML = ("Temperature: " + convertTemp + " °C")
-            Wind.innerHTML = ("Wind: " + windValue + " Kph");
-            Humid.innerHTML = ("Humidity: " + HumidValue);
-
-
-            createImage.src= "http://openweathermap.org/img/wn/" + ImgValue + ".png";
-            
-            document.getElementById('Index').append(createImage);
-
-            HistoryCities.push(CityValue);
-            localStorage.setItem('saved', JSON.stringify(HistoryCities));
-            addEntry(); 
-        })
-
-
-
-    .catch(err => alert("Wrong city name"))
-
-    // fetch request to get 5-day forecast
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + Input.value + '&units=imperial&appid=20516ee7fdcc8a804bdcda9f63d8d30a')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-                var Icon1 = 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon+ '.png';
-                $('#Icon1').attr('src', Icon1);
-                document.getElementById("Temp1" ).innerHTML = 'Temp: ' + Number(data.list[0].main.temp).toFixed(0) + "°C";
-                document.getElementById("Wind1").innerHTML = 'Wind: ' + Number(data.list[0].wind.speed) + " Kph";
-                document.getElementById("Humidity1").innerHTML = 'Humidity: ' + Number(data.list[0].main.humidity);
-                document.getElementById("Date1").innerHTML = (data.list[0].dt_txt);
-
-                var Icon2 = 'https://openweathermap.org/img/w/' + data.list[8].weather[0].icon+ '.png';
-                $('#Icon2').attr('src', Icon2);
-                document.getElementById("Temp2" ).innerHTML = 'Temp: ' + Number(data.list[8].main.temp).toFixed(0) + "°C";
-                document.getElementById("Wind2").innerHTML = 'Wind: ' + Number(data.list[8].wind.speed) + " Kph";
-                document.getElementById("Humidity2").innerHTML = 'Humidity: ' + Number(data.list[8].main.humidity);
-                document.getElementById("Date2").innerHTML = (data.list[8].dt_txt);
-
-                var Icon3 = 'https://openweathermap.org/img/w/' + data.list[16].weather[0].icon+ '.png';
-                $('#Icon3').attr('src', Icon3);
-                document.getElementById("Temp3" ).innerHTML = 'Temp: ' + Number(data.list[16].main.temp).toFixed(0) + "°C";
-                document.getElementById("Wind3").innerHTML = 'Wind: ' + Number(data.list[16].wind.speed) + " Kph";
-                document.getElementById("Humidity3").innerHTML = 'Humidity: ' + Number(data.list[16].main.humidity);
-                document.getElementById("Date3").innerHTML = (data.list[16].dt_txt);
-
-                var Icon4 = 'https://openweathermap.org/img/w/' + data.list[24].weather[0].icon+ '.png';
-                $('#Icon4').attr('src', Icon4);
-                document.getElementById("Temp4" ).innerHTML = 'Temp: ' + Number(data.list[24].main.temp).toFixed(0) + "°C";
-                document.getElementById("Wind4").innerHTML = 'Wind: ' + Number(data.list[24].wind.speed) + " Kph";
-                document.getElementById("Humidity4").innerHTML = 'Humidity: ' + Number(data.list[24].main.humidity);
-                document.getElementById("Date4").innerHTML = (data.list[24].dt_txt);
-
-                var Icon5 = 'https://openweathermap.org/img/w/' + data.list[32].weather[0].icon+ '.png';
-                $('#Icon5').attr('src', Icon5);
-                document.getElementById("Temp5" ).innerHTML = 'Temp: ' + Number(data.list[32].main.temp).toFixed(0) + "°C";
-                document.getElementById("Wind5").innerHTML = 'Wind: ' + Number(data.list[32].wind.speed) + " Kph";
-                document.getElementById("Humidity5").innerHTML = 'Humidity: ' + Number(data.list[32].main.humidity);
-                document.getElementById("Date5").innerHTML = (data.list[32].dt_txt);
-        })
-
-});
-
-
-$('#BtnSpots').on('click', '#Relook' , function (event) { 
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-     console.log("Clicked");
-     var city = $(this).text();
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=20516ee7fdcc8a804bdcda9f63d8d30a')
-    .then(response => response.json())
-    .then(data => {
-        //console.log(data);
-        var CityValue = data['name'];
-        var TemperValue = data['main']['temp'];
-        var windValue = data['wind']['speed'];
-        var HumidValue = data['main']['humidity'];
-        var ImgValue = data.weather[0].icon;
-
-
-        CityName.innerHTML = (CityValue + " ");
-        var convertTemp = Math.trunc(1.8 * (TemperValue - 273) + 32);
-        Temp.innerHTML = ("Temperature: " + convertTemp + " °C");
-        Wind.innerHTML = ("Wind: " + windValue + " Kph");
-        Humid.innerHTML = ("Humidity: " + HumidValue);
-
-
-        createImage.src= "http://openweathermap.org/img/wn/" + ImgValue + ".png";
-        
-        document.getElementById('Index').append(createImage);
-
-        HistoryCities.push(CityValue);
-        localStorage.setItem('saved', JSON.stringify(HistoryCities));
-        addEntry(); 
-    })
-
-
-
-.catch(err => alert("Wrong city name"))
-
-// fetch request to get 5-day forecast
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=20516ee7fdcc8a804bdcda9f63d8d30a')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-            var Icon1 = 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon+ '.png';
-            $('#Icon1').attr('src', Icon1);
-            document.getElementById("Temp1" ).innerHTML = 'Temp: ' + Number(data.list[0].main.temp).toFixed(0) + "°C";
-            document.getElementById("Wind1").innerHTML = 'Wind: ' + Number(data.list[0].wind.speed) + " Kph";
-            document.getElementById("Humidity1").innerHTML = 'Humidity: ' + Number(data.list[0].main.humidity);
-            document.getElementById("Date1").innerHTML = (data.list[0].dt_txt);
-
-            var Icon2 = 'https://openweathermap.org/img/w/' + data.list[8].weather[0].icon+ '.png';
-            $('#Icon2').attr('src', Icon2);
-            document.getElementById("Temp2" ).innerHTML = 'Temp: ' + Number(data.list[8].main.temp).toFixed(0) + "°C";
-            document.getElementById("Wind2").innerHTML = 'Wind: ' + Number(data.list[8].wind.speed) + " Kph";
-            document.getElementById("Humidity2").innerHTML = 'Humidity: ' + Number(data.list[8].main.humidity);
-            document.getElementById("Date2").innerHTML = (data.list[8].dt_txt);
-
-            var Icon3 = 'https://openweathermap.org/img/w/' + data.list[16].weather[0].icon+ '.png';
-            $('#Icon3').attr('src', Icon3);
-            document.getElementById("Temp3" ).innerHTML = 'Temp: ' + Number(data.list[16].main.temp).toFixed(0) + "°C";
-            document.getElementById("Wind3").innerHTML = 'Wind: ' + Number(data.list[16].wind.speed) + " Kph";
-            document.getElementById("Humidity3").innerHTML = 'Humidity: ' + Number(data.list[16].main.humidity);
-            document.getElementById("Date3").innerHTML = (data.list[16].dt_txt);
-
-            var Icon4 = 'https://openweathermap.org/img/w/' + data.list[24].weather[0].icon+ '.png';
-            $('#Icon4').attr('src', Icon4);
-            document.getElementById("Temp4" ).innerHTML = 'Temp: ' + Number(data.list[24].main.temp).toFixed(0) + "°C";
-            document.getElementById("Wind4").innerHTML = 'Wind: ' + Number(data.list[24].wind.speed) + " Kph";
-            document.getElementById("Humidity4").innerHTML = 'Humidity: ' + Number(data.list[24].main.humidity);
-            document.getElementById("Date4").innerHTML = (data.list[24].dt_txt);
-
-            var Icon5 = 'https://openweathermap.org/img/w/' + data.list[32].weather[0].icon+ '.png';
-            $('#Icon5').attr('src', Icon5);
-            document.getElementById("Temp5" ).innerHTML = 'Temp: ' + Number(data.list[32].main.temp).toFixed(0) + "°C";
-            document.getElementById("Wind5").innerHTML = 'Wind: ' + Number(data.list[32].wind.speed) + " Kph";
-            document.getElementById("Humidity5").innerHTML = 'Humidity: ' + Number(data.list[32].main.humidity);
-            document.getElementById("Date5").innerHTML = (data.list[32].dt_txt);
-    })
-
-});
-
-
-
-$ClearBtn.on('click', function () {
-    localStorage.clear();
-    location.reload();
 })
+
+.catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"))
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "London";
+    GetInfo();
+}
+
+
+//Getting and displaying the text for the upcoming five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }

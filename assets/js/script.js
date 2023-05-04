60c40460cd0db5a280abcfc12925a3dc17715b5c
MichealHamoud <micheal.hamoud11@outlook.com>
2023-05-04 18:33:40 +1000

    re-wrote the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,29 @@
 function GetInfo() {
+    const cityName = document.getElementById("cityName");
+    const newName = document.getElementById("cityInput").value;
+    cityName.innerHTML = `--${newName}--`;
 
-    var newName = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--";
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=20516ee7fdcc8a804bdcda9f63d8d30a')
-.then(response => response.json())
-.then(data => {
-
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
-
-    }
-
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
-    }
-     for(i = 0; i<5; i++){
-        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
-        data.list[i].weather[0].icon
-        +".png";
-    }
-    console.log(data)
-
-
-})
-
-.catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"))
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newName}&appid=20516ee7fdcc8a804bdcda9f63d8d30a`)
+    .then(response => response.json())
+    .then(data => {
+        const {list} = data;
+        for(let i = 0; i < 5; i++){
+            document.getElementById(`day${i+1}Min`).innerHTML = `Min: ${(list[i].main.temp_min - 273.15).toFixed(1)}°`;
+            document.getElementById(`day${i+1}Max`).innerHTML = `Max: ${(list[i].main.temp_max - 273.15).toFixed(2)}°`;
+            document.getElementById(`img${i+1}`).src = `http://openweathermap.org/img/wn/${list[i].weather[0].icon}.png`;
+        }
+    })
+    .catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"));
 }
 
 function DefaultScreen(){
-    document.getElementById("cityInput").defaultValue = "melbourne";
+    document.getElementById("cityInput").value = "melbourne";
     GetInfo();
 }
 
-
-//Getting and displaying the text for the upcoming five days of the week
-var d = new Date();
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
-
-//Function to get the correct integer for the index of the days array
-function CheckDay(day){
-    if(day + d.getDay() > 6){
-        return day + d.getDay() - 7;
-    }
-    else{
-        return day + d.getDay();
-    }
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+for(let i = 0; i < 5; i++){
+    const dayIndex = (i + d.getDay()) % 7;
+    document.getElementById(`day${i+1}`).innerHTML = weekdays[dayIndex];
 }
-
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
-    }
